a25a3bdddc7589afb5d2384b70e5db481228abd6
Plinio Nascimento <plinio.gsnascimento@gmail.com>
2023-05-04 22:45:46 -0300

    Adding egresstest

--- manifests.tf
+++ manifests.tf
@@ -1,6 +1,7 @@
 locals {
   bookinfo_apps    = split("---", file("${path.module}/istio/samples/bookinfo/platform/kube/bookinfo.yaml"))
   bookinfo_gateway = split("---", file("${path.module}/istio/samples/bookinfo/networking/bookinfo-gateway.yaml"))
+  egresstest       = split("---", file("${path.module}/manifests/egresstest.yaml"))
 }
 
 resource "kubectl_manifest" "bookinfo-app" {
@@ -12,5 +13,11 @@ resource "kubectl_manifest" "bookinfo-app" {
 resource "kubectl_manifest" "bookinfo-gateway" {
   count      = length(local.bookinfo_gateway)
   yaml_body  = local.bookinfo_gateway[count.index]
+  depends_on = [helm_release.istio_base, helm_release.istio_discovery]
+}
+
+resource "kubectl_manifest" "egresstest" {
+  count      = length(local.egresstest)
+  yaml_body  = local.egresstest[count.index]
   depends_on = [helm_release.istio_base, helm_release.istio_discovery]
 }
